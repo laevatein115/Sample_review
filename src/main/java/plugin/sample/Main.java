d2340c80660316a5289dfd52be27b3b732ecb3f6
laevatein115 <sakuya.nadesiko@gmail.com>
2025-09-16 23:28:43 +0900

    ログイン時にメッセージが出るようにコードを追加

--- src/main/java/plugin/sample/Main.java
+++ src/main/java/plugin/sample/Main.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,6 +19,11 @@ public final class Main extends JavaPlugin implements Listener {
   @Override
   public void onEnable() {
     Bukkit.getPluginManager().registerEvents(this, this);
+  }
+
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent e) {
+    e.setJoinMessage(e.getPlayer().getName() + "　がログインしました");
   }
 
   /**
