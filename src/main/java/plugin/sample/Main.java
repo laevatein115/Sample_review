58ebd6d7ccf4ad5f7d143fda1d610ec8724f2e12
laevatein115 <sakuya.nadesiko@gmail.com>
2025-09-24 22:53:50 +0900

    Join時に指定のEntityをspawnするコードを追加

--- src/main/java/plugin/sample/Main.java
+++ src/main/java/plugin/sample/Main.java
@@ -4,7 +4,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.FireworkEffect.Type;
+import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Chicken;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -27,7 +29,12 @@ public final class Main extends JavaPlugin implements Listener {
 
   @EventHandler
   public void onPlayerJoin(PlayerJoinEvent e) {
-    e.setJoinMessage(e.getPlayer().getName() + "　がログインしました");
+    Player player = e.getPlayer();
+    World world = player.getWorld();
+
+    Location playerLocation = player.getLocation();
+
+    world.spawn(new Location(world, playerLocation.getX() + 3, playerLocation.getY(), playerLocation.getZ()), Chicken.class);
   }
 
   /**
